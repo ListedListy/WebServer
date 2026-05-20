8b87896d400024c0fa7af175651f762de1d2d15f
Listy <p4nd7s@gmail.com>
2026-05-20 16:02:06 +0100

    Desenvolvimento das páginas principais

--- social/urls.py
+++ social/urls.py
@@ -5,3 +5,8 @@ from . import views
 app_name = 'social'
 
 urlpatterns = []
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('utilizadores/', views.users, name='users'),
+    path('utilizadores/<str:username>/', views.user_posts, name='user_posts'),
+]
--- social/views.py
+++ social/views.py
@@ -1,3 +1,64 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+SAMPLE_USERS = [
+    {
+        'username': 'nathan',
+        'name': 'Nathan Santos',
+        'bio': 'Aluno de administracao de sistemas e desenvolvimento web.',
+    },
+    {
+        'username': 'maria',
+        'name': 'Maria Costa',
+        'bio': 'Gosta de bases de dados, Linux e cafe forte.',
+    },
+]
+
+SAMPLE_POSTS = [
+    {
+        'id': 1,
+        'author': SAMPLE_USERS[0],
+        'title': 'Primeiro deploy em Django',
+        'body': 'Hoje comecei a preparar a aplicacao para correr em ambientes separados.',
+        'likes': 8,
+        'comments': 2,
+    },
+    {
+        'id': 2,
+        'author': SAMPLE_USERS[1],
+        'title': 'Docker Compose simplifica tudo',
+        'body': 'Um ficheiro bem organizado ajuda a levantar a app e a base de dados rapidamente.',
+        'likes': 12,
+        'comments': 4,
+    },
+    {
+        'id': 3,
+        'author': SAMPLE_USERS[0],
+        'title': 'Variaveis de ambiente',
+        'body': 'Segredos e configuracoes devem ficar fora do codigo versionado.',
+        'likes': 5,
+        'comments': 1,
+    },
+]
+
+
+def home(request):
+    return render(request, 'social/home.html', {'posts': SAMPLE_POSTS})
+
+
+def users(request):
+    return render(request, 'social/users.html', {'users': SAMPLE_USERS})
+
+
+def user_posts(request, username):
+    user = next((item for item in SAMPLE_USERS if item['username'] == username), None)
+    posts = [post for post in SAMPLE_POSTS if post['author']['username'] == username]
+
+    return render(
+        request,
+        'social/user_posts.html',
+        {
+            'profile': user,
+            'posts': posts,
+        },
+    )
